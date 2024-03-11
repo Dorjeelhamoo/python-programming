e9de4e18c2175fcc5b6bfcd0b15db3d574a21d52
dorjeelhamoo <141301170+Dorjeelhamoo@users.noreply.github.com>
2024-03-11 20:34:54 +0600

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,35 @@
+x = "Hello world" #stri
+x = 50 
+x = 60.5
+x = 3j
+x = ["geeks", "for", "geeks"]
+x = ("geeks", "for", "geeks")
+x = range(10)
+x = {"name": "suraj", "age": 24}
+x = {"geeks", "for", "geeks"}
+x = frozenset({"geeks", "for", "geeks"})
+x = True
+x = b"Geeks"
+x = bytearray(4)
+x = memoryview(bytes(6))
+x = None
+
+my_list = [1, 2, 3, 4, 5, "strings"]
+my_list.insert(1, 5)
+print(my_list)
+
+my_list.append(5)
+my_list.append(8)
+print(my_list)
+
+print(len(my_list))
+
+length = len(my_list)
+print (length)
+
+my_list.remove(my_list[0])
+print(my_list)
+
+my_list.pop()
+print(my_list)
+
