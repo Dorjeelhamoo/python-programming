9723f04c3b2d22a66b0e358ae21d1df64c2c45bb
dorjeelhamoo <141301170+Dorjeelhamoo@users.noreply.github.com>
2024-03-11 20:37:00 +0600

    Add files via upload

---
+++ string manipulation.py
@@ -0,0 +1,27 @@
+my_city = "'New York'"
+print(type(my_city))
+
+my_city = 'New York'
+print(type(my_city))
+
+my_city = str("New York")
+print(type(my_city))
+
+word1 = 'New'
+word2 = 'York'
+print(word1 + word2)
+
+word = " Rio de Janeiro "
+char = word[0]
+print(char)
+
+x=word.replace('Rio', 'Mar')
+print(x)
+
+print(word.count('0'))
+
+print(word * 3)
+
+split_word = word.split(" ")
+print(split_word)
+
